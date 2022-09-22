e09274c2f2cccf80569e7e13c8cbee40c680e18b
Kaloyan Angelov <75050098+kaloyannbg@users.noreply.github.com>
2022-09-22 12:01:18 +0300

    Create single.php

---
+++ single.php
@@ -0,0 +1,26 @@
+                    <script>
+                    function showMapFunction() {
+                    document.getElementById("map-acf").style = "none";
+                    }
+                    </script>
+
+                    <div class="detail-content">
+                        <?php
+                            $mapLocation = get_field('map_location'); 
+                            
+                        ?>
+                        <div class="acf-map" id="map-acf" style="overflow:auto">
+                            <?php
+                                if(!empty($mapLocation['lat']))                  
+                                    echo '<div class="marker" data-lat="'.$mapLocation['lat'].'" data-lng="'.$mapLocation['lng'].'">
+                                    <div style="color: black;">'.esc_html( $mapLocation['address']).'</div></div>';
+                            ?>
+                        </div>
+                        <div class="map-box" onclick="showMapFunction()">Show Map</div>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <?php get_template_part('template-parts/content', 'single'); ?>
+                        <?php endwhile; // End of the loop. 
+                        ?>
+                        <?php comments_template(); ?>
+
+                    </div><!-- /.end of deatil-content -->
